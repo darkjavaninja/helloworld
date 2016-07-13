3aa27bc8f1ba29d02dd01f73f4087c17a6b372d8
darkjavaninja <darkjavaninja@gmail.com>
2016-07-13 18:00:37 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	public static String hello (String name) {
+		
+			switch(name){
+				case  "" : 
+				case "null":
+			
+				
+				return "Hello, World!";
+				
+				default : 
+				return ("Hello, " + name + "!");
+			}
+	
+	}
+}
